a6ca1dded0d3dcdba25c2163cec39e4f23e47202
BensonWang221 <wangce3356@163.com>
2022-02-12 16:52:26 +0800

    config.h.in

--- tutorial.cxx
+++ tutorial.cxx
@@ -2,11 +2,14 @@
 #include <cstdlib>
 #include <iostream>
 #include <string>
+#include "TutorialConfig.h"
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
     if (argc < 2)
     {
+        std::cout << argv[0] << " version " << Tutorial_VERSION_MAJOR
+                  << "." << Tutorial_VERSION_MINOR << std::endl;
         std::cout << "Usage: " << argv[0] << " number" << std::endl;
         return 1;
     }
@@ -16,6 +19,6 @@ int main(int argc, char* argv[])
     const double result = sqrt(inputVal);
     std::cout << "The square root of " << argv[1] << " is " << result
               << std::endl;
-    
+
     return 0;
 }
